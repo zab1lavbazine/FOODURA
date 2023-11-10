4389fb5a2e6b0e2ec7299cc2d89c537df13972c6
bareldan <bareldan@fit.cvut.cz>
2023-11-10 09:41:11 +0100

    testing

--- Server-Java/src/test/java/com/example/serverjava/ServerJavaApplicationTests.java
+++ Server-Java/src/test/java/com/example/serverjava/ServerJavaApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.serverjava;
 
+import com.example.serverjava.DTO.UserINFO;
+import com.example.serverjava.Entity.User;
+import com.example.serverjava.Repository.UserRepository;
+import com.example.serverjava.Service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class ServerJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private UserService userService;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Test
+    public void testGetUserById() {
+        // Mocking UserRepository behavior
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testuser");
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        User foundUser = userService.getUserById(1L);
+
+        assertEquals("testuser", foundUser.getUsername());
+    }
 
 }
