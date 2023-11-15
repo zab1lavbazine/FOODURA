740fa318543c75613f2c3dfc4070845ffeb94c8e
bareldan <bareldan@fit.cvut.cz>
2023-11-15 23:39:39 +0100

    adding new test

--- Server-Java/src/test/java/com/example/serverjava/ServerJavaApplicationTests.java
+++ Server-Java/src/test/java/com/example/serverjava/ServerJavaApplicationTests.java
@@ -1,20 +1,27 @@
 package com.example.serverjava;
 
+import com.example.serverjava.DTO.OrderINFO;
+import com.example.serverjava.DTO.ProductINFO;
 import com.example.serverjava.DTO.UserINFO;
+import com.example.serverjava.Entity.Order;
+import com.example.serverjava.Entity.Product;
 import com.example.serverjava.Entity.User;
+import com.example.serverjava.Repository.ProductRepository;
 import com.example.serverjava.Repository.UserRepository;
+import com.example.serverjava.Service.ProductService;
 import com.example.serverjava.Service.UserService;
-import org.junit.jupiter.api.BeforeEach;
+
+import org.hibernate.mapping.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.List;
 import java.util.Optional;
 
+import javax.management.relation.Role;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -23,8 +30,14 @@ class ServerJavaApplicationTests {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ProductService productService;
+
     @MockBean
     private UserRepository userRepository;
+
+    @MockBean
+    private ProductRepository productRepository;
 
     @Test
     public void testGetUserById() {
@@ -37,6 +50,22 @@ class ServerJavaApplicationTests {
         User foundUser = userService.getUserById(1L);
 
         assertEquals("testuser", foundUser.getUsername());
+    }
+
+    @Test
+    public void addNewProduct() {
+
+        Product newProduct = new Product();
+        newProduct.setDescription("test");
+        newProduct.setName("test");
+        newProduct.setPrice(10);
+        newProduct.setId(1L);
+
+        when(productRepository.findById(newProduct.getId())).thenReturn(Optional.of(newProduct));
+
+        Product foundProduct = productService.getProductById(newProduct.getId());
+
+        assertEquals(newProduct, foundProduct);
     }
 
 }
