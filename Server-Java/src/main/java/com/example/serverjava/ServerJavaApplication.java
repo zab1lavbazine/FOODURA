be7e554eff5b3b548f476a3fc4eed528dbdba4c4
bareldan <bareldan@fit.cvut.cz>
2023-11-20 22:33:38 +0100

    adding new file for test

--- Server-Java/src/main/java/com/example/serverjava/ServerJavaApplication.java
+++ Server-Java/src/main/java/com/example/serverjava/ServerJavaApplication.java
@@ -1,12 +1,10 @@
 package com.example.serverjava;
 
-import com.example.serverjava.Config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(AppConfig.class)
 public class ServerJavaApplication {
 
 
