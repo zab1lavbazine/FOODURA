ef5dbcdcd75674c6794d4946624e95bc0de09ef7
bareldan <bareldan@fit.cvut.cz>
2024-01-01 19:58:20 +0100

    update

--- src/main/java/com/example/serverjava/Entity/SupportEntity/OrderWithProductsRequest.java
+++ src/main/java/com/example/serverjava/Entity/SupportEntity/OrderWithProductsRequest.java
@@ -1,13 +1,11 @@
 package com.example.serverjava.Entity.SupportEntity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
